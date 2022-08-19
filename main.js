e0b762bfc926c38a0d0d2505d3cc4206a954b44f
ShelbyJenkins <shelbyjenkins@gmail.com>
2022-08-19 18:11:39 -0500

    Add features to site

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-document.querySelector('video').setAttribute("height",  window.innerHeight + 'px');
+window.addEventListener('click', loadMain);
+
+// sets and maintains perfectly scaled height of video and site width
+document.getElementById('cityVideo').style.height = window.innerHeight + 'px';
+document.querySelector('video').style.height = window.innerHeight + 'px';
+// document.querySelector('body').style.width = window.innerWidth + 'px';
+
 window.addEventListener('resize', function() {
-    document.querySelector('video').setAttribute("height",  window.innerHeight + 'px');
-    console.log('Window resized to ' + window.innerHeight + 'px');
+    document.getElementById('cityVideo').style.height = window.innerHeight + 'px';
+    document.querySelector('video').style.height = window.innerHeight + 'px';
+    // document.querySelector('body').style.width = window.innerWidth + 'px';
 });
+
+
+
+    
+function loadMain() {
+    console.log('test');
+    window.removeEventListener('click', loadMain);
+    document.getElementById('intro').style.display = 'none';
+    document.getElementById('main').style.display = 'flex';
+    // Sets background color to white for intro transition.
+    window.onLoad = document.querySelector('body').style = "background-color: white";
+}
+
+
