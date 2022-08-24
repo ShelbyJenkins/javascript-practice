22ec8fa4a0b31392c79e83aa0b3eb136073b3771
ShelbyJenkins <shelbyjenkins@gmail.com>
2022-08-24 17:21:41 -0500

    // Fixes spacing issue and adds additional comments.

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
+// Click anywhere to load main page.
 window.addEventListener('click', loadMain);
 
+function loadMain() {
+    // Generates effects for transition from intro to main.
+    randomTyping();
+    document.getElementById('intro').classList.add('intro-container-spin-fade');
+    // Removes EL once main page is loaded to prevent repeat reload.
+    window.removeEventListener('click', loadMain);
+    // Loads main page once transition ends.
+    setTimeout(function() {
+        document.body.style.overflow = 'auto';
+        document.getElementById('intro').style.display = 'none';
+        document.getElementById('main').style.display = 'flex';
+        document.getElementById('main').style.backgroundColor = 'white';
+        document.body.style.backgroundColor = 'white';
+        }, 2500);
+}
 
 // Randomly generates increasing text for a screaming trip through a portal.
+// Must be async due to the interaction between the for loop and the timeout.
 const timer = ms => new Promise(res => setTimeout(res, ms))
-async function randomTyping () { 
-    document.getElementById('intro-content').innerHTML = 'A';  
-     // This section increase text size and container width per iteration
+async function randomTyping () {   
+    // Grabs the elements before the loop is initiated 
     txt = document.getElementById('intro-content');
-    fSize = window.getComputedStyle(txt, null).getPropertyValue('font-size');
-    fS = parseInt(fSize);
+    fS = parseInt(window.getComputedStyle(txt, null).getPropertyValue('font-size'));
     cnt = document.getElementsByClassName('intro-container')[0]
-    cWidth = window.getComputedStyle(cnt, null).getPropertyValue('width');
-    cW = parseInt(cWidth);
+    cW = parseInt(window.getComputedStyle(cnt, null).getPropertyValue('width'));
     for (var i = 0; i < 60; i++) {
+        // Increases the width of the container per iteration.
         document.getElementsByClassName('intro-container')[0].style.width =+ 100 + '%'; 
-        // This section increase text size and container width per iteration
-        fS += 6;
-        cW += 50;
-        txt.style.fontSize = fS + 'px';
-        cnt.style.width = cW + 'px';
-        // This section generates an 'a' or 'A' at random.
+        // Increases text size per iteration
+        txt.style.fontSize = (fS += 6) + 'px';
+        cnt.style.width = (cW += 50) + 'px';
+        // This section generates an 'a' or 'A' at random. Switch is cleaner than if.
         let randomNumber = Math.floor(Math.random() * 3);
         switch (randomNumber) {
             case 0:
@@ -30,18 +43,4 @@ async function randomTyping () {
         await timer(50); 
         }
     }
-}
-
-function loadMain() {
-    randomTyping();
-    window.removeEventListener('click', loadMain);
-    document.getElementById('intro').classList.add('intro-container-spin-fade');
-    setTimeout(function() {
-        document.body.style.overflow = 'auto';
-        document.getElementById('intro').style.display = 'none';
-        document.getElementById('main').style.display = 'flex';
-        // Sets background color to white for intro transition.
-        document.getElementById('main').style.backgroundColor = 'white';
-        document.body.style.backgroundColor = 'white';
-        }, 2500);
 }
--- readme.txt
+++ readme.txt
@@ -1,4 +1,6 @@
+Just a portfolio.
 
+Built with:
 https://nostalgic-css.github.io/NES.css/#
 https://github.com/nostalgic-css/NES.css
 
